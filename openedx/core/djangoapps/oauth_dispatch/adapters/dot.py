ca168d0cd5bb362f238fe18767c8df77f3c0c0c2
Douglas Hall <douglas_hall@icloud.com>
2018-06-26 05:15:31 -0400

    Fix user filter.

--- openedx/core/djangoapps/oauth_dispatch/adapters/dot.py
+++ openedx/core/djangoapps/oauth_dispatch/adapters/dot.py
@@ -93,6 +93,12 @@ class DOTAdapter(object):
         """
         application = self.get_client(client_id=client_id)
         filters = [org_relation.to_jwt_filter_claim() for org_relation in application.organizations.all()]
-        if application.authorization_grant_type == application.GRANT_CLIENT_CREDENTIALS:
+
+        # Allow applications configured with the client credentials grant type to access
+        # data for all users. This will allow clients to fetch data in bulk.
+        # Applications configured with all other grant types should only have access
+        # to data for the request user.
+        if application.authorization_grant_type != application.GRANT_CLIENT_CREDENTIALS:
             filters.append(self.FILTER_USER_ME)
+
         return filters
